20199f181b4db9feb690f529a82cc62f1005b4ce
SeWonKim <30452963+Siromom@users.noreply.github.com>
2020-05-06 01:30:06 +0900

    [Feat] 데이터 연동
    - useEffect로 data fetch 하기 [블로그 참조](https://www.robinwieruch.de/react-hooks-fetch-data)
      두번째 인자로 오는 배열은 컴포넌트가 업데이트 될 때마다 실행되는 게 아니라 마운트 될 때만 실행 되도록 한다.

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,8 @@
 /* API를 요청하고 뉴스데이터가 들어 있는 배열을 컴포넌트 배열로 변환하여 렌더링하는 컴포넌트 */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import NewsItem from './NewsItem';
 
 const NewsListBlock = styled.div`
@@ -14,26 +15,40 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const sampleArticle = {
-  title: '제목',
-  description: '내용',
-  url: 'https://google.com',
-  urlToImage: 'https://via.placeholder.com/160',
-};
+function NewsList() {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-function NewsList(props) {
+  // Fetch data when the component mounts
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          'http://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey=b540785b10d44283a7805f8e87785008',
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>loading...</NewsListBlock>;
+  }
+  if (!articles) {
+    return null;
+  }
+
   return (
     <NewsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
     </NewsListBlock>
   );
 }
