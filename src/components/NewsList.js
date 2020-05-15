bf97ebeaaa6398f8dae58622219185693cade63e
Siromom <swon962@gmail.com>
2020-05-15 15:43:16 +0900

    usePromise라는 Hook을 만들어 사용
    NewsList에서 API를 호출 할 때처럼 Promise를 사용하는 경우 코드를 간결하게 하기 .위해서 커스텀 Hook을 만들어 적용
    대기 중 상태관리, useEffect 설정을 직접 하지 않아도 되기 때문에 코드가 간결 해진다.

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,9 +1,10 @@
 /* API를 요청하고 뉴스데이터가 들어 있는 배열을 컴포넌트 배열로 변환하여 렌더링하는 컴포넌트 */
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import NewsItem from './NewsItem';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   display: flex;
@@ -22,36 +23,25 @@ const NewsListBlock = styled.div`
 `;
 
 function NewsList({ category }) {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  // Fetch data when the component mounts
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-
-        const response = await axios.get(
-          `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=b540785b10d44283a7805f8e87785008`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=b540785b10d44283a7805f8e87785008`,
+    );
   }, [category]);
 
   if (loading) {
     return <NewsListBlock>loading...</NewsListBlock>;
   }
-  if (!articles) {
+  if (!response) {
     return null;
   }
+  if (error) {
+    return <NewsListBlock>Error!</NewsListBlock>;
+  }
 
+  // response 값이 유효할 때
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
